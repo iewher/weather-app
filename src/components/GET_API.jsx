eb3fc72e0635352ff5568b4d0ce92587b4e5e656
Georgy <goshagang@mail.ru>
2023-04-22 04:25:25 +0500

    temperature request added

--- src/components/GET_API.jsx
+++ src/components/GET_API.jsx
@@ -4,7 +4,8 @@ import axios from 'axios'
 const API_KEY = '88e144c5d2e3e012c2db6b8be6114ff8';
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
-function GET_API(city) {
+function GET_API(city, temp) {
+
   axios.get(API_URL, {
     params: {
       q: city,
@@ -15,15 +16,14 @@ function GET_API(city) {
   .then(response => {
     console.log(response.data);
   })
+  .then(response => {
+    console.log(response.data);
+    const temp = response.data.main.temp;
+    console.log(`Temperature in ${city}: ${temp}°C`);
+  })
   .catch(error => {
     console.log(error);
   });
-
-  return (
-    <div>
-      
-    </div>
-  );
 }
 
 export default GET_API;
