58fca2dc2f4c281c48b4aa6e95f229f3dc99bf0a
Georgy <goshagang@mail.ru>
2023-04-22 06:58:34 +0500

    added callback

--- src/components/GET_API.jsx
+++ src/components/GET_API.jsx
@@ -4,8 +4,7 @@ import axios from 'axios'
 const API_KEY = '88e144c5d2e3e012c2db6b8be6114ff8';
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
-function GET_API(city, temp) {
-
+function GET_API(city, callback) {
   axios.get(API_URL, {
     params: {
       q: city,
@@ -14,12 +13,15 @@ function GET_API(city, temp) {
     },
   })
   .then(response => {
-    console.log(response.data);
-  })
-  .then(response => {
-    console.log(response.data);
-    const temp = response.data.main.temp;
-    console.log(`Temperature in ${city}: ${temp}°C`);
+    if (response) {
+      console.log(response.data)
+      const temp = response.data.main.temp;
+      const windSpeed = response.data.wind.speed;
+      const weather = response.data.weather[0].main;
+      callback(temp, windSpeed, weather);
+    } else {
+      console.log('Response is undefined');
+    }
   })
   .catch(error => {
     console.log(error);
