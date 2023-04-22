3fd2c334e095456f8cb548e6c94ecffd3334ff89
Georgy <goshagang@mail.ru>
2023-04-22 21:22:06 +0500

    add comments for GET_API.jsx

--- src/components/GET_API.jsx
+++ src/components/GET_API.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import axios from 'axios'
 
+/*
+Обьявляеем переменные API_KEY и API_URL
+В API_KEY передаем значение токена
+В API_URL передаем ссылку, куда будем делать запрос
+*/
+
 const API_KEY = '88e144c5d2e3e012c2db6b8be6114ff8';
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+/*
+Фукнция принимает два параметра
+city - название города, для которого нужно получить данные
+callback - фукнция обратного вызова, которая будет выполнена после получения ответа от API
+*/
 
 function GET_API(city, callback) {
   axios.get(API_URL, {
@@ -13,8 +25,19 @@ function GET_API(city, callback) {
     },
   })
   .then(response => {
+
+/*
+Обязательно проверяем поступление данных
+Если данные не поступили, обрабатывает ошибку
+*/
+
     if (response) {
       console.log(response.data)
+
+/*
+Обращаемся к данным, которые нам вернул API запрос
+*/
+
       const temp = response.data.main.temp;
       const feelsLike = response.data.main.feels_like
       const windSpeed = response.data.wind.speed;
