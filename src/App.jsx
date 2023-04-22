d0c6a7643d2d28b920f0a4a8d95a09a8f4e8feb0
Georgy <goshagang@mail.ru>
2023-04-22 21:21:39 +0500

    add comments for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import Container from './components/container'
 
 function App() {
   return (
+    
+    /*
+    Вызываю контейнер, в котором находится все содержимое
+    */
+
     <Container />
   );
 }
